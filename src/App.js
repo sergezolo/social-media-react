7ba250798ea3e9682ef7be9bd300d23a54de6226
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-10-20 22:50:03 -0500

    Show profiles

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import AuthRoute from './util/AuthRoute';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import user from './pages/user';
 
 const theme = createTheme({
   // palette: {
@@ -62,6 +63,7 @@ export class App extends Component {
                   <Route exact path="/" component={ home }/>
                   <AuthRoute exact path="/login" component={ login } />
                   <AuthRoute exact path="/signup" component={ signup } />
+                  <Route exact path="/users/:handle" component={ user }/>
                 </Switch>
               </div>
             </Router>
